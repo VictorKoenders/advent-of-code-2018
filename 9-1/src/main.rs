514c31d479febcc19f03e01ee65336984e47e285
Victor Koenders <victor.koenders@gmail.com>
2018-12-11 10:23:44 +0000

    Switched to a VecDeque and direct iterator for a 7x performance increase

--- 9-1/src/main.rs
+++ 9-1/src/main.rs
@@ -1,29 +1,25 @@
+use std::collections::VecDeque;
+
 fn main() {
     let player_count: usize = std::env::args().nth(1).unwrap().parse().unwrap();
     let marble_count: usize = std::env::args().nth(2).unwrap().parse().unwrap();
 
     println!("{} players, {} marbles", player_count, marble_count);
 
-    let mut remaining_marbles: Vec<usize> = (0..=marble_count).collect();
-    let mut ring_marbles: Vec<usize> = Vec::with_capacity(marble_count);
-    let mut players = vec![Vec::new(); player_count];
+    let mut ring_marbles: VecDeque<usize> = VecDeque::with_capacity(marble_count);
+    let mut players = vec![0usize; player_count];
     let mut ring_marble_index = 0usize;
 
-    for player_index in (0..player_count).cycle() {
-        if remaining_marbles.is_empty() { break; }
-
-        let marble = remaining_marbles.remove(0);
-
+    for (marble, player_index) in (0..=marble_count).zip((0..player_count).cycle()) {
         if marble % 23 == 0 && marble != 0 {
             let mut removing_marble_index = ring_marble_index as isize - 9;
             while removing_marble_index < 0 {
                 removing_marble_index += ring_marbles.len() as isize;
             }
             let removing_marble_index = removing_marble_index as usize;
-            let removed_marble = ring_marbles.remove(removing_marble_index);
+            let removed_marble = ring_marbles.remove(removing_marble_index).unwrap();
 
-            players[player_index].push(marble);
-            players[player_index].push(removed_marble);
+            players[player_index] += marble + removed_marble;
 
             ring_marble_index = removing_marble_index;
         } else {
@@ -36,10 +32,9 @@ fn main() {
     }
 
     let mut highest = (0, 0);
-    for (index, score) in players.into_iter().enumerate().filter(|(_, s)| !s.is_empty()) {
-        let sum: usize = score.iter().sum();
-        if sum > highest.1 {
-            highest = (index, sum);
+    for (index, score) in players.into_iter().enumerate() {
+        if score > highest.1 {
+            highest = (index, score);
         }
     }
 
